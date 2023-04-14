3f2424cef3a700bed0fd19ae441bb1298687f557
arou1990 <arou1990@gmail.com>
2023-04-13 23:32:28 -0400

    added routers

--- RottenPotatoes/src/app/app.module.ts
+++ RottenPotatoes/src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+ // app.module.ts
+
+ import appRoutes from './routerconfig';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    MainPageComponent,
+    ProfilePageComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+],
   providers: [],
   bootstrap: [AppComponent]
 })

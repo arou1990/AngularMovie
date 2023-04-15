3c0b55d39856a8acb9e62ab610c2d03e8c257bd2
arou1990 <arou1990@gmail.com>
2023-04-14 21:50:11 -0400

    Header built just needs spacing

--- RottenPotatoes/src/app/app.module.ts
+++ RottenPotatoes/src/app/app.module.ts
@@ -4,21 +4,33 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
  // app.module.ts
 
- import appRoutes from './routerconfig';
+import appRoutes from './routerconfig';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { ReviewDialogComponent } from './review-dialog/review-dialog.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
     MainPageComponent,
-    ProfilePageComponent
+    ProfilePageComponent,
+    HeaderComponent,
+    ReviewDialogComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
 ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ RottenPotatoes/src/app/review-dialog/review-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-review-dialog',
+  templateUrl: './review-dialog.component.html',
+  styleUrls: ['./review-dialog.component.css']
+})
+export class ReviewDialogComponent {
+
+}
